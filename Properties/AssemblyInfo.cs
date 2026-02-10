29edd185b20468312fa2638fb0731692fa76fa6a
hzb1130 <626061157@qq.com>
2026-02-10 17:51:42 +0800

    AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using MelonLoader;
+using ItemScanner;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ItemScan")]
+[assembly: AssemblyDescription("Display markers on nearby items")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ItemScan")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d4c2b9c0-0b95-4151-b3be-6e94b8f3e8ec")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(ItemScanner.ItemScanner), "Item Scanner", "1.0.0", "HZB")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
